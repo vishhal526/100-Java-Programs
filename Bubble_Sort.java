f9d16492d21984df1c674e57d2ea62c1cf53b300
vishhal526 <vishhjha@gmail.com>
2025-04-28 22:23:56 +0530

    Remove unwanted .class files and clean repository

--- Bubble_Sort.java
+++ Bubble_Sort.java
@@ -5,6 +5,17 @@ class Bubble_Sort {
 
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter the length of Array");
+        int len = s.nextInt();
+
+        int[] a = new int[len];
+
+        for(int i=0; i<len; i++){
+            System.out.print("Enter "+(i+1)+" element = ");
+            a[i] = s.nextInt();
+        }
+
         
     }
 }
