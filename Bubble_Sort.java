0cc6dbf11600f1a4843beff7e712f4f445d3ccba
vishhal526 <vishhjha@gmail.com>
2025-04-29 16:20:12 +0530

    Added Bubble Sort

--- Bubble_Sort.java
+++ Bubble_Sort.java
@@ -6,16 +6,36 @@ class Bubble_Sort {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        System.out.print("Enter the length of Array");
+        System.out.print("\nEnter the length of Array = ");
         int len = s.nextInt();
 
         int[] a = new int[len];
 
-        for(int i=0; i<len; i++){
-            System.out.print("Enter "+(i+1)+" element = ");
+        for (int i = 0; i < len; i++) {
+            System.out.print("Enter " + (i + 1) + " element = ");
             a[i] = s.nextInt();
         }
 
-        
+        for (int i = 0; i < len; i++) {
+
+            for (int j = 1; j < (len - i); j++) {
+
+                if (a[j] < a[j - 1]) {
+
+                    int x = a[j];
+                    a[j] = a[j - 1];
+                    a[j - 1] = x;
+
+                }
+
+            }
+        }
+
+        System.out.print("Sorted Array = ");
+
+        for (int i = 0; i < len; i++) {
+            System.out.print(a[i] + " ");
+        }
+
     }
 }
